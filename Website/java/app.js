775615533e7997d678ba06c3c7f9619b3f81f290
Kristen Malone <kristen.marie.malone@gmail.com>
2015-06-07 15:12:29 -0400

    stuck! but added..

---
+++ Website/java/app.js
@@ -0,0 +1,50 @@
+var main = function() {
+
+  // window.onload = function() {
+   
+  // removed this bc Steph taught me the better trick with "display: none" and "display: block" in .css
+  // $('.lie').hide();
+  	 
+  // };
+
+  $('.cost').click(function() {
+    
+    var thisSection = $(this);
+
+    $('.costView').removeClass('current');
+    $('.lie').hide();
+    
+    $(this).addClass('current');
+    $(this).children('.costView').addClass('current')
+    $(this).children('.lie').show();
+
+    //$('.lie').hide(function(){
+      //  thisSection.children('.lie').addClass('selected');
+      //  thisSection.children('.costView').addClass('current');
+    //});
+
+    // $(this).children('.lie').toggleClass('selected');
+    // $(this).children('.costView').toggleClass('current');
+
+    // $(this).addClass('current');
+    // $(this).children('.costView').addClass('current')
+    // $(this).children('.lie').show();
+
+  });
+
+  <!---- trying to hide children when you click again on the current cost ---->
+    
+
+
+
+  $('.btn').click (function() {
+  	var post = $('.lie-box').val();
+  	$('<li>').text(post).prependTo('.posts');
+  	$('.lie-box').val(' ');
+  });
+
+
+
+}
+
+$(document).ready(main);
